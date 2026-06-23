3868f6919a4c3dd55c5c9d5b6b299c6fb5ec90ec
pavan-dubey369 <pavan.dubey@pivotroots.com>
2026-06-23 15:21:08 +0530

    Fix middlewares CORS to use process.env instead of env()

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,7 +9,7 @@ module.exports = [
         'http://localhost:5173',
         'http://localhost:3000',
         'http://localhost:3001',
-        env('FRONTEND_URL', ''),
+        process.env.FRONTEND_URL,
       ].filter(Boolean),
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization'],
