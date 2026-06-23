57ab8f2a7c0d2202ec503b3f138b4c919949fa1d
pavan-dubey369 <pavan.dubey@pivotroots.com>
2026-06-23 17:24:09 +0530

    Set CORS origin to wildcard for all origins

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,18 +5,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: (ctx, callback) => {
-        const origin = ctx.request?.header?.origin || ctx.header?.origin || '';
-        const allowed = [
-          'http://localhost:5173',
-          'http://localhost:3000',
-          'http://localhost:3001',
-          process.env.FRONTEND_URL || '',
-        ];
-        const isVercel = origin.endsWith('.vercel.app');
-        const isAllowed = isVercel || allowed.includes(origin);
-        callback(null, isAllowed ? origin : false);
-      },
+      origin: '*',
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization'],
     },
