1f3f347136efd18df993fff702fee62c08858794
pavan-dubey369 <pavan.dubey@pivotroots.com>
2026-06-23 12:09:34 +0530

    Initial Strapi backend for expense tracker
    - Category and Expense content types
    - CORS configured for frontend
    - SQLite database setup

---
+++ config/middlewares.js
@@ -0,0 +1,19 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:3001'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
